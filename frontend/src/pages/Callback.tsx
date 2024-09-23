8673e9768e7ca9be247d873f43de442a7d5c3063
Steve Chapman <steve.g.chapman@gmail.com>
2024-09-24 00:05:11 +1200

    drop use of navigate in callback

--- frontend/src/pages/Callback.tsx
+++ frontend/src/pages/Callback.tsx
@@ -1,10 +1,8 @@
 import React, { useEffect } from 'react';
-import { useNavigate } from 'react-router-dom';
 import { useAuth } from '../integration/AuthContext';
 import UnAuthenticated from './Unauthenticated';
 
 const Callback: React.FC = () => {
-  const navigate = useNavigate();
   const {
     /* no need to use anything here */
   } = useAuth();
@@ -12,8 +10,7 @@ const Callback: React.FC = () => {
   useEffect(() => {
     // The actual handling is done in AuthContext
     // This component can show a loading state
-    navigate('/user-notes');
-  }, [navigate]);
+  }, []);
 
   console.log('handling callback');
   return (
